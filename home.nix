59d8b1428236f4e4842cbea3872c11aba4072853
Lucie <gnosticstate@gmail.com>
2024-05-04 22:22:42 +0200

    Added VSCode.

--- home.nix
+++ home.nix
@@ -145,4 +145,44 @@
         size = 12;
       };
     };
+
+    # Configure Visual Studio Code
+    programs.vscode = {
+
+      # Enable VS Code
+      enable = true;
+
+      # Install extensions
+      extensions = with pkgs.vscode-extensions; [
+
+        # Utility
+        ms-vsliveshare.vsliveshare
+        shardulm94.trailing-spaces
+        vscodevim.vim
+        xaver.clang-format
+
+        # Languages
+        bbenoist.nix
+        haskell.haskell
+        ms-vscode.cpptools
+        ms-dotnettools.csharp
+        yzhang.markdown-all-in-one
+      ];
+    };
+
+    # Add config file for VS Code
+    xdg.configFile."Code/User/settings.json" = {
+      text = ''
+        {
+          "editor.fontFamily": "'FiraCode Nerd Font', monospace",
+          "editor.tabSize": 2,
+          "editor.wordWrap": "wordWrapColumn",
+          "explorer.confirmDelete": false,
+          "git.confirmSync": false,
+          "window.menuBarVisibility": "toggle",
+          "window.zoomLevel": 1,
+          "workbench.startupEditor": "none",
+        }
+      '';
+    };
 }
