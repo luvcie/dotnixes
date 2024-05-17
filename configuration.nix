ba944ef198625cb56ce6d541df8191a849098644
Lucie <gnosticstate@gmail.com>
2024-05-17 04:33:30 +0200

    enabled kde-connect

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,20 @@ services.printing = {
   ];
 };
 
+    # Open ports required for KDE Connect
+  networking.firewall = { 
+    enable = true;
+    allowedTCPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+    allowedUDPPortRanges = [ 
+      { from = 1714; to = 1764; } # KDE Connect
+    ];  
+  };  
+
+  programs.kdeconnect.enable = true; 
+
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
