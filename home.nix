4088219600d725513affd9ea24aa906e2db17289
Lucie <gnosticstate@gmail.com>
2024-06-29 23:18:08 +0200

    formatted with alejandra

--- home.nix
+++ home.nix
@@ -1,139 +1,140 @@
-{ inputs, config, pkgs, ... }:
-
 {
+  inputs,
+  config,
+  pkgs,
+  ...
+}: {
   # Let Home Manager install and manage itself.
-	programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
   home = {
     username = "lucie";
     homeDirectory = "/home/lucie";
 
     packages = with pkgs; [
+      # Flake inputs
+      inputs.lobster.packages."x86_64-linux".lobster #This is broken because of the API limit but hopefully it will be fixed one day.
 
-			# Flake inputs
-			inputs.lobster.packages."x86_64-linux".lobster #This is broken because of the API limit but hopefully it will be fixed one day.
-
-			# Packages
-			alejandra
-			android-tools
-			anbox
-			ani-cli
-			any-nix-shell
-			appimage-run
-			audacity
-			bat # A cat clone with syntax highlighting and Git integration.
-			bitwarden
-			blahaj
-			cargo # Manage Rust projects and their module dependencies (crates).
-			cbonsai
-			clisp
-			clojure
-			cmatrix
-			cowsay
-			cups
-			chromium
-			cwiid
-			deluge
-			discordo
-			emacs
-			extremetuxracer
-			figlet
-			freedroidrpg
-			friture
-			flameshot
-			hugo # A fast and modern static website engine.
-			jdk
-			foliate
-			leiningen
-			libnotify
-			librewolf
-			lukesmithxyz-bible-kjv
-			micro
-			minetest
-			mov-cli
-			ncdu # A disk usage analyzer with an interactive interface.
-			nchat # Terminal-based chat client with support for Telegram and WhatsApp
-			neovim
-			nheko
-			nicotine-plus # Sovlseek
-			obs-studio
-			pacvim
-			prismlauncher
-			porsmo # Pomodoro written in Rust.
-			qemu
-			retroshare
-			sbcl # LISP compiler
-			scanmem
-			sl
-			stardust # Space flight simulator.
-			signal-desktop
-			superTux
-			superTuxKart
-			speedtest-go
-			telegram-desktop
-			tetrio-desktop
-			termdown # Countdown cli program.
-			tomato-c # Pomodoro written in pure C.
-			tgpt
-			vesktop # Discord
-			vimb
-			vimgolf
-			vitetris
-			vlc
-			kdePackages.kdeconnect-kde
-			hollywood
-			waydroid
-			waylyrics
-			webcord-vencord
-			wipeout-rewrite
-			wl-clipboard # Command-line copy/paste utilities for Wayland.
-			qbittorrent
-			xwiimote
-			yt-dlp
-			terraform
-			libgcc
-			yazi
-			virtualenv
-			python312Packages.pip
-			python3
-			# # # Newly added:
-			blanket # play background sounds
-			bleachbit # program to clean computer
-			dissent # GTK4 Discord client in Go
-			iperf # tool to measure IP bandwidth using UDP or TCP
-			mtr-gui # network diagnostics tool
-			remmina # remote desktop client
-			shellcheck # shell script analysis tool
-			shortwave # find and listen to internet radio stations
-			tcpdump # network sniffer
-			trashy # simple, fast, and featureful alternative to rm and trash-cli
-			tuba # browse the Fediverse
-			rPackages.VedicDateTime
-			iotop
-			theharvester
-			krabby
-			macchina
-			asciicam
-			fswebcam
-			libwebcam
-			webcamoid
-			gst_all_1.gstreamer
-			v4l2-relayd
-			gpt4all
-			# # # Pentesting
-			# 802-11
-			aircrack-ng # wireless WEP/WPA cracking utilities
-			bully # Implementation of the WPS brute force attack, written in C
-			cowpatty # Brute-force WPA dictionary attack
-			hashcat # World's fastest and most advanced password recovery utility
-			iw # tool for configuring Linux wireless devices
-			macchanger # utility for manipulating the MAC address of network interfaces	
-			pixiewps # Offline WPS bruteforce tool
-			reaverwps-t6x # brute force attack tool against Wifi Protected Setup PIN number
-			wifite2 # Python script to automate wireless auditing using aircrack-ng tools
-			wordlists # collection of wordlists useful for security testing
-			
-		];
+      # Packages
+      alejandra
+      android-tools
+      anbox
+      ani-cli
+      any-nix-shell
+      appimage-run
+      audacity
+      bat # A cat clone with syntax highlighting and Git integration.
+      bitwarden
+      blahaj
+      cargo # Manage Rust projects and their module dependencies (crates).
+      cbonsai
+      clisp
+      clojure
+      cmatrix
+      cowsay
+      cups
+      chromium
+      cwiid
+      deluge
+      discordo
+      emacs
+      extremetuxracer
+      figlet
+      freedroidrpg
+      friture
+      flameshot
+      hugo # A fast and modern static website engine.
+      jdk
+      foliate
+      leiningen
+      libnotify
+      librewolf
+      lukesmithxyz-bible-kjv
+      micro
+      minetest
+      mov-cli
+      ncdu # A disk usage analyzer with an interactive interface.
+      nchat # Terminal-based chat client with support for Telegram and WhatsApp
+      neovim
+      nheko
+      nicotine-plus # Sovlseek
+      obs-studio
+      pacvim
+      prismlauncher
+      porsmo # Pomodoro written in Rust.
+      qemu
+      retroshare
+      sbcl # LISP compiler
+      scanmem
+      sl
+      stardust # Space flight simulator.
+      signal-desktop
+      superTux
+      superTuxKart
+      speedtest-go
+      telegram-desktop
+      tetrio-desktop
+      termdown # Countdown cli program.
+      tomato-c # Pomodoro written in pure C.
+      tgpt
+      vesktop # Discord
+      vimb
+      vimgolf
+      vitetris
+      vlc
+      kdePackages.kdeconnect-kde
+      hollywood
+      waydroid
+      waylyrics
+      webcord-vencord
+      wipeout-rewrite
+      wl-clipboard # Command-line copy/paste utilities for Wayland.
+      qbittorrent
+      xwiimote
+      yt-dlp
+      terraform
+      libgcc
+      yazi
+      virtualenv
+      python312Packages.pip
+      python3
+      # # # Newly added:
+      blanket # play background sounds
+      bleachbit # program to clean computer
+      dissent # GTK4 Discord client in Go
+      iperf # tool to measure IP bandwidth using UDP or TCP
+      mtr-gui # network diagnostics tool
+      remmina # remote desktop client
+      shellcheck # shell script analysis tool
+      shortwave # find and listen to internet radio stations
+      tcpdump # network sniffer
+      trashy # simple, fast, and featureful alternative to rm and trash-cli
+      tuba # browse the Fediverse
+      rPackages.VedicDateTime
+      iotop
+      theharvester
+      krabby
+      macchina
+      asciicam
+      fswebcam
+      libwebcam
+      webcamoid
+      gst_all_1.gstreamer
+      v4l2-relayd
+      gpt4all
+      # # # Pentesting
+      # 802-11
+      aircrack-ng # wireless WEP/WPA cracking utilities
+      bully # Implementation of the WPS brute force attack, written in C
+      cowpatty # Brute-force WPA dictionary attack
+      hashcat # World's fastest and most advanced password recovery utility
+      iw # tool for configuring Linux wireless devices
+      macchanger # utility for manipulating the MAC address of network interfaces
+      pixiewps # Offline WPS bruteforce tool
+      reaverwps-t6x # brute force attack tool against Wifi Protected Setup PIN number
+      wifite2 # Python script to automate wireless auditing using aircrack-ng tools
+      wordlists # collection of wordlists useful for security testing
+    ];
 
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
@@ -144,94 +145,90 @@
     # release notes.
     stateVersion = "23.11"; # Please read the comment before changing.
   };
-	nixpkgs.config.allowUnfreePredicate = _: true;
-	programs.git.enable = true;
-	programs.starship.enable = true;
-	programs.fzf.enable = true;
-	
-	programs.zsh = {
-		enable = true;
-		enableCompletion = true;
-		syntaxHighlighting.enable = true;
-		enableVteIntegration = true;
-		shellAliases = {
-			run-with-xwayland = "env -u WAYLAND_DISPLAY";
-		};
+  nixpkgs.config.allowUnfreePredicate = _: true;
+  programs.git.enable = true;
+  programs.starship.enable = true;
+  programs.fzf.enable = true;
 
-		# Install plugins
-		plugins = [
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    enableVteIntegration = true;
+    shellAliases = {
+      run-with-xwayland = "env -u WAYLAND_DISPLAY";
+    };
 
-			# Vi keybindings
-			{
-				name = "zsh-vi-mode";
-				file = "./share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
-				src = pkgs.zsh-vi-mode;
-			}
+    # Install plugins
+    plugins = [
+      # Vi keybindings
+      {
+        name = "zsh-vi-mode";
+        file = "./share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+        src = pkgs.zsh-vi-mode;
+      }
 
-			# Autosuggestions
-			{
-				name = "zsh-autosuggestions";
-				file = "./share/zsh-autosuggestions/zsh-autosuggestions.zsh";
-				src = pkgs.zsh-autosuggestions;
-			}
-		];
-	};
-	# Configure kitty terminal
-	programs.kitty = {
+      # Autosuggestions
+      {
+        name = "zsh-autosuggestions";
+        file = "./share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+        src = pkgs.zsh-autosuggestions;
+      }
+    ];
+  };
+  # Configure kitty terminal
+  programs.kitty = {
+    # Enable kitty
+    enable = true;
 
-		# Enable kitty
-		enable = true;
-
-		# Configure font
-		font = {
-			name = "FiraCode Nerd Font";
-			size = 12;
-		};
-	};
+    # Configure font
+    font = {
+      name = "FiraCode Nerd Font";
+      size = 12;
+    };
+  };
 
   # Magnificent app that corrects your previous console command, integrates with zsh.
-	programs.thefuck.enable = true;
+  programs.thefuck.enable = true;
 
   # Enable zoxide
-	programs.zoxide.enable = true;
+  programs.zoxide.enable = true;
 
-	# Configure Visual Studio Code
-	programs.vscode = {
+  # Configure Visual Studio Code
+  programs.vscode = {
+    # Enable VS Code
+    enable = true;
 
-		# Enable VS Code
-		enable = true;
+    # Install extensions
+    extensions = with pkgs.vscode-extensions; [
+      # Utility
+      ms-vsliveshare.vsliveshare
+      shardulm94.trailing-spaces
+      vscodevim.vim
+      xaver.clang-format
 
-		# Install extensions
-		extensions = with pkgs.vscode-extensions; [
+      # Languages
+      bbenoist.nix
+      haskell.haskell
+      ms-vscode.cpptools
+      ms-dotnettools.csharp
+      yzhang.markdown-all-in-one
+    ];
+  };
 
-			# Utility
-			ms-vsliveshare.vsliveshare
-			shardulm94.trailing-spaces
-			vscodevim.vim
-			xaver.clang-format
-
-			# Languages
-			bbenoist.nix
-			haskell.haskell
-			ms-vscode.cpptools
-			ms-dotnettools.csharp
-			yzhang.markdown-all-in-one
-		];
-	};
-
-	# Add config file for VS Code
-	xdg.configFile."Code/User/settings.json" = {
-		text = ''
-			{
-				"editor.fontFamily": "'FiraCode Nerd Font', monospace",
-				"editor.tabSize": 2,
-				"editor.wordWrap": "wordWrapColumn",
-				"explorer.confirmDelete": false,
-				"git.confirmSync": false,
-				"window.menuBarVisibility": "toggle",
-				"window.zoomLevel": 1,
-				"workbench.startupEditor": "none",
-			}
-		'';
-	};
+  # Add config file for VS Code
+  xdg.configFile."Code/User/settings.json" = {
+    text = ''
+      {
+      	"editor.fontFamily": "'FiraCode Nerd Font', monospace",
+      	"editor.tabSize": 2,
+      	"editor.wordWrap": "wordWrapColumn",
+      	"explorer.confirmDelete": false,
+      	"git.confirmSync": false,
+      	"window.menuBarVisibility": "toggle",
+      	"window.zoomLevel": 1,
+      	"workbench.startupEditor": "none",
+      }
+    '';
+  };
 }
