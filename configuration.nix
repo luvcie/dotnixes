68d692a7259071270a74912411daf548395b3c5e
Lucie <gnosticstate@gmail.com>
2024-07-19 16:32:57 +0200

    Added audiness and openVAS

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,10 @@
   pkgs,
   ...
 }: {
+
+  # Kernel version
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
--- home.nix
+++ home.nix
@@ -16,6 +16,8 @@
       inputs.lobster.packages."x86_64-linux".lobster #This is broken because of the API limit but hopefully it will be fixed one day.
 
       # Packages
+      openvas-scanner
+      audiness
       vcv-rack
       flamegraph
       linuxKernel.packages.linux_xanmod_latest.perf
