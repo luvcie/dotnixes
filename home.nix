c898104b5ffb013802e7800c949a9acfb4ee41bd
Lucie <gnosticstate@gmail.com>
2024-08-15 22:51:37 +0200

    nix flake update

--- home.nix
+++ home.nix
@@ -16,6 +16,7 @@
       inputs.lobster.packages."x86_64-linux".lobster #This is broken because of the API limit but hopefully it will be fixed one day.
 
       # Packages
+      ladybird
       xboxdrv
       protonmail-desktop
       trustymail
