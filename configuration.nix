3ce94dd3cc34c8a4dde88ca8949b67a386ca53d0
luvcie <lucpardo@student.42.fr>
2025-05-12 01:56:08 +0200

    modularize audio

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports = [
     ./hardware-configuration.nix
     ./pksay.nix
+	./modules/audio.nix
   ];
 
   #######################
@@ -205,124 +206,9 @@
     CLUTTER_BACKEND = "wayland";
     XDG_SESSION_TYPE = "wayland";
   };
-  #######################
-  # AUDIO CONFIGURATION #
-  #######################
-
-  security.rtkit.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-
-    extraConfig.pipewire = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 1024;
-        "default.clock.min-quantum" = 1024;
-        "default.clock.max-quantum" = 1024;
-        "core.clock.power-of-two-quantum" = true;
-
-        "core.daemon" = true;
-        "core.name" = "pipewire-0";
-        "mem.allow-mlock" = true;
-
-        "node.latency" = "256/48000";
-        "vm.overcommit" = true;
-
-        "default.clock.allowed-rates" = [44100 48000 88200 96000];
-
-        "core.recovery.time" = 10000;
-
-        "default.fragments" = 2;
-        "default.fragment.size-max" = 4096;
-
-        "default.format" = "F32";
-        "default.position" = ["FL" "FR"];
-
-        "support.dbus" = true;
-        "log.level" = 2;
-
-        "pulse.min.req" = 256;
-        "pulse.default.req" = 256;
-        "pulse.max.req" = 256;
-        "pulse.min.frag" = 256;
-        "pulse.default.frag" = 256;
-        "pulse.max.frag" = 256;
-      };
-
-      "context.modules" = [
-        {
-          name = "libpipewire-module-rtkit";
-          args = {
-            "nice.level" = -20;
-            "rt.prio" = 99;
-            "rt.time.soft" = 2000000;
-            "rt.time.hard" = 2000000;
-          };
-          flags = ["ifexists" "nofail"];
-        }
-        {
-          name = "libpipewire-module-protocol-pulse";
-          args = {
-            "pulse.min.req" = "256/48000";
-            "pulse.default.req" = "256/48000";
-            "pulse.max.req" = "256/48000";
-            "pulse.min.quantum" = "256/48000";
-            "pulse.max.quantum" = "256/48000";
-          };
-        }
-      ];
-    };
-  };
 
   security.pam = {
     loginLimits = [
-      {
-        domain = "@audio";
-        item = "memlock";
-        type = "-";
-        value = "unlimited";
-      }
-      {
-        domain = "@audio";
-        item = "rtprio";
-        type = "-";
-        value = "99";
-      }
-      {
-        domain = "@audio";
-        item = "nofile";
-        type = "soft";
-        value = "99999";
-      }
-      {
-        domain = "@audio";
-        item = "nofile";
-        type = "hard";
-        value = "99999";
-      }
-      {
-        domain = "@audio";
-        item = "nice";
-        type = "-";
-        value = "-20";
-      }
-      {
-        domain = "@realtime";
-        item = "nice";
-        type = "-";
-        value = "-20";
-      }
-      {
-        domain = "@realtime";
-        item = "rtprio";
-        type = "-";
-        value = "99";
-      }
       {
         domain = "*";
         item = "memlock";
@@ -462,7 +348,6 @@
 
   environment.systemPackages = with pkgs; [
     ly
-    pavucontrol
     usbutils
     udiskie
     udisks
@@ -515,8 +400,6 @@
       "networkmanager"
       "wheel"
       "docker"
-      "audio"
-      "realtime"
       "video"
       "input"
       "render"
---
+++ modules/audio.nix
@@ -0,0 +1,145 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config.modules.audio;
+in
+{
+  options.modules.audio = {
+    enable = lib.mkEnableOption "Custom audio configuration using PipeWire, RTKit, and PAM limits";
+    userName = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "The primary user that should be added to audio-related groups.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    #######################
+    # AUDIO CONFIGURATION #
+    #######################
+
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+
+      extraConfig.pipewire = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 1024;
+          "default.clock.min-quantum" = 1024;
+          "default.clock.max-quantum" = 1024;
+          "core.clock.power-of-two-quantum" = true;
+
+          "core.daemon" = true;
+          "core.name" = "pipewire-0";
+          "mem.allow-mlock" = true;
+
+          "node.latency" = "256/48000";
+          "vm.overcommit" = true;
+
+          "default.clock.allowed-rates" = [ 44100 48000 88200 96000 ];
+
+          "core.recovery.time" = 10000;
+
+          "default.fragments" = 2;
+          "default.fragment.size-max" = 4096;
+
+          "default.format" = "F32";
+          "default.position" = [ "FL" "FR" ];
+
+          "support.dbus" = true;
+          "log.level" = 2;
+
+          "pulse.min.req" = 256;
+          "pulse.default.req" = 256;
+          "pulse.max.req" = 256;
+          "pulse.min.frag" = 256;
+          "pulse.default.frag" = 256;
+          "pulse.max.frag" = 256;
+        };
+
+        "context.modules" = [
+          {
+            name = "libpipewire-module-rtkit";
+            args = {
+              "nice.level" = -20;
+              "rt.prio" = 99;
+              "rt.time.soft" = 2000000;
+              "rt.time.hard" = 2000000;
+            };
+            flags = [ "ifexists" "nofail" ];
+          }
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              "pulse.min.req" = "256/48000";
+              "pulse.default.req" = "256/48000";
+              "pulse.max.req" = "256/48000";
+              "pulse.min.quantum" = "256/48000";
+              "pulse.max.quantum" = "256/48000";
+            };
+          }
+        ];
+      };
+    };
+
+    security.pam.loginLimits = [
+      {
+        domain = "@audio";
+        item = "memlock";
+        type = "-";
+        value = "unlimited";
+      }
+      {
+        domain = "@audio";
+        item = "rtprio";
+        type = "-";
+        value = "99";
+      }
+      {
+        domain = "@audio";
+        item = "nofile";
+        type = "soft";
+        value = "99999";
+      }
+      {
+        domain = "@audio";
+        item = "nofile";
+        type = "hard";
+        value = "99999";
+      }
+      {
+        domain = "@audio";
+        item = "nice";
+        type = "-";
+        value = "-20";
+      }
+      {
+        domain = "@realtime";
+        item = "nice";
+        type = "-";
+        value = "-20";
+      }
+      {
+        domain = "@realtime";
+        item = "rtprio";
+        type = "-";
+        value = "99";
+      }
+    ];
+
+    environment.systemPackages = with pkgs; [
+      pavucontrol
+    ];
+
+    users.users = lib.mkIf (cfg.userName != null && config.users.users."${cfg.userName}" != null) {
+      "${cfg.userName}".extraGroups = [ "audio" "realtime" ];
+    };
+  };
+}
