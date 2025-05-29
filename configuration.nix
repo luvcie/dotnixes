371e631fa2abc9c07e06faa3d779251551f8dcb5
luvcie <lucpardo@student.42.fr>
2025-05-29 15:45:33 +0200

    add flatpak

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     ./hardware-configuration.nix
     ./pksay.nix
 	./modules/audio.nix
+	./modules/kernel.nix
   ];
 
   #######################
@@ -38,54 +39,7 @@
   nixpkgs.config.allowUnfree = true;
 
   services.gvfs.enable = true;
-
-  #######################
-  #   boot & hardware   #
-  #######################
-
-  boot = {
-    kernelPackages = pkgs.linuxPackages_zen;
-
-    loader = {
-      systemd-boot = {
-        enable = true;
-        consoleMode = "max";
-        editor = false;
-      };
-      efi.canTouchEfiVariables = true;
-      timeout = 3;
-    };
-
-    kernelParams = [
-      "amdgpu.gpu_recovery=1"
-      "transparent_hugepage=never"
-    ];
-
-    kernel.sysctl = {
-      "vm.swappiness" = 10;
-      "vm.vfs_cache_pressure" = 50;
-      "vm.max_map_count" = 262144;
-
-      "kernel.sched_autogroup_enabled" = 0;
-      "kernel.sched_child_runs_first" = 1;
-
-      "fs.file-max" = 2097152;
-      "fs.inotify.max_user_watches" = 524288;
-
-      "net.core.rmem_max" = 2500000;
-      "net.core.wmem_max" = 2500000;
-      "net.core.rmem_default" = 1048576;
-      "net.core.wmem_default" = 1048576;
-      "net.ipv4.tcp_fastopen" = 3;
-      "net.ipv4.tcp_congestion_control" = "bbr";
-
-      "kernel.pid_max" = 4194304;
-
-      "kernel.unprivileged_userns_clone" = 1;
-    };
-
-    kernelModules = ["binder_linux" "ashmem_linux"];
-  };
+  services.flatpak.enable = true;
 
   #######################
   #  DISPLAY & DESKTOP  #
